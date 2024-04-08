a0bed69c79f7eb35756924bf0dd6470745a47705
Sklaerenn <69436470+5klaerenn@users.noreply.github.com>
2024-04-08 16:47:07 -0400

    notes with input

---
+++ part2/notes/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import Note from "./components/Note";
+
+const App = (props) => {
+
+	const [notes, setNotes] = useState(props.notes)	
+
+	const addNote = (e) => {
+		e.preventDefault();
+		console.log("button clicked", e.target);
+	}
+
+	 return (
+			<div>
+				<h1>Notes</h1>
+				<ul>
+					{notes.map((note) => (
+						<Note key={note.id} note={note} />
+					))}
+				</ul>
+				<form onSubmit={addNote}>
+					<input />
+					<button type="submit">save</button>
+				</form>
+			</div>
+		);
+};
+
+export default App;
+
